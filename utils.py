4be942c07041397aa27054da17233c5d53ee18a5
pavithra.v22_infosys.com#EXT#@idemoppc.onmicrosoft.com <pavithra.v22_infosys.com#EXT#@idemoppc.onmicrosoft.com>
2023-02-11 14:17:07 +0000

    model

--- utils.py
+++ utils.py
@@ -1,124 +1,202 @@
-'''Some helper functions for PyTorch, including:
-    - get_mean_and_std: calculate the mean and std value of dataset.
-    - msr_init: net parameter initialization.
-    - progress_bar: progress bar mimic xlua.progress.
-'''
-import os
-import sys
-import time
-import math
-
+from __future__ import print_function
+import torch
 import torch.nn as nn
-import torch.nn.init as init
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import torchvision
+import numpy as np
+from torch.optim.lr_scheduler import StepLR, OneCycleLR
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+from tqdm import tqdm
+from pytorch_grad_cam import GradCAM, HiResCAM, ScoreCAM, GradCAMPlusPlus, AblationCAM, XGradCAM, EigenCAM, FullGrad
+from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
+from pytorch_grad_cam.utils.image import show_cam_on_image
+from torchvision.models import resnet50
+import matplotlib.pyplot as plt
+from torchsummary import summary
+
+wrong_img =[]
+pred_label =[]
+target_label=[]
+wrong_preds=[]
+
+def get_device():
+  use_cuda = torch.cuda.is_available()
+  device = torch.device("cuda" if use_cuda else "cpu")
+  return device
+
+def apply_albumentations(img,mean,std):
+    img =img.permute(1, 2, 0)
+    img=np.array(img)
+    aug = A.Compose([
+	#A.Cutout(num_holes=1, max_h_size=5, max_w_size=5, always_apply=True, p=0.2),
+	 A.CoarseDropout(max_holes=1,
+                       min_holes =1,
+                       max_height=5, 
+                       max_width=5, 
+                       p=0.2,
+                       fill_value= mean,
+                       min_height=5, min_width=5, always_apply=False)
+    ,A.PadIfNeeded(min_height=36, min_width=36, p=0.2)
+    ,A.RandomCrop(32, 32, always_apply=True, p=0.2)
+  #,A.Normalize(mean=mean, std=std, always_apply=True)
+    ,ToTensorV2()
+
+   ])
+    augmented_img = aug(image=img)['image']
+    return augmented_img
+
+def get_transform():
+  return transforms.Compose([
+     transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
 
 
-def get_mean_and_std(dataset):
-    '''Compute the mean and std value of dataset.'''
-    dataloader = torch.utils.data.DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
-    mean = torch.zeros(3)
-    std = torch.zeros(3)
-    print('==> Computing mean and std..')
-    for inputs, targets in dataloader:
-        for i in range(3):
-            mean[i] += inputs[:,i,:,:].mean()
-            std[i] += inputs[:,i,:,:].std()
-    mean.div_(len(dataset))
-    std.div_(len(dataset))
-    return mean, std
+def get_transform1():
+  return transforms.Compose([
+     transforms.ToTensor()
+])
 
-def init_params(net):
-    '''Init layer parameters.'''
-    for m in net.modules():
-        if isinstance(m, nn.Conv2d):
-            init.kaiming_normal(m.weight, mode='fan_out')
-            if m.bias:
-                init.constant(m.bias, 0)
-        elif isinstance(m, nn.BatchNorm2d):
-            init.constant(m.weight, 1)
-            init.constant(m.bias, 0)
-        elif isinstance(m, nn.Linear):
-            init.normal(m.weight, std=1e-3)
-            if m.bias:
-                init.constant(m.bias, 0)
+def get_trainset(transform):
+  # Load the CIFAR-10 dataset
+  return datasets.CIFAR10(root='./data', train=True,
+                                          download=True, transform=transform )
+
+def get_testloader(transform):
+  testset= torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+  return torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2,pin_memory=True)
+
+def get_summary(model):
+  use_cuda = torch.cuda.is_available()
+  device = torch.device("cuda" if use_cuda else "cpu")
+  summary(model, input_size=(3, 32, 32))
 
 
-_, term_width = os.popen('stty size', 'r').read().split()
-term_width = int(term_width)
+def train(model, device, trainloader, optimizer,scheduler, epoch,lastepoch,loss_l1 =False):
+  model.train()
+  
+  pbar = tqdm(trainloader)
+  
+  correct = 0
+  processed = 0
+  for batch_idx, (data, target) in enumerate(pbar):
+    
+    # get samples
+    data, target = data.to(device), target.to(device)
+    # Init
+    optimizer.zero_grad()
+    
+    # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
+    # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
 
-TOTAL_BAR_LENGTH = 65.
-last_time = time.time()
-begin_time = last_time
-def progress_bar(current, total, msg=None):
-    global last_time, begin_time
-    if current == 0:
-        begin_time = time.time()  # Reset for new bar.
+    # Predict
+    y_pred = model(data)
+    
+    # Calculate loss
 
-    cur_len = int(TOTAL_BAR_LENGTH*current/total)
-    rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
+    loss = F.nll_loss(y_pred, target)
+    if(loss_l1):
+      l1=0
+      lamba_l1 = 1e-5
+      for p in model.parameters():
+        l1=l1+p.abs().sum()
+      loss = loss+lamba_l1*l1
+  
+    # Backpropagation
+    loss.backward()
+    optimizer.step()
+    scheduler.step()
+    # Update pbar-tqdm
+    
+    pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+    correct += pred.eq(target.view_as(pred)).sum().item()
+    processed += len(data)
+    pbar.set_description(desc= f'Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+    if epoch == lastepoch-1 :
+      wrong_preds = [i for i, x in enumerate(pred.eq(target.view_as(pred))) if x == False]
+      tar = target.reshape(-1, 1)
+      for index_val in wrong_preds:
+        if len(wrong_img) < 10:
+          wrong_img.append(data[index_val].cpu().numpy().squeeze())
+          target_label.append(tar[index_val].cpu().numpy().squeeze())
+          pred_label.append(pred[index_val].cpu().numpy().squeeze())
 
-    sys.stdout.write(' [')
-    for i in range(cur_len):
-        sys.stdout.write('=')
-    sys.stdout.write('>')
-    for i in range(rest_len):
-        sys.stdout.write('.')
-    sys.stdout.write(']')
+def test(model, device, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum().item()
 
-    cur_time = time.time()
-    step_time = cur_time - last_time
-    last_time = cur_time
-    tot_time = cur_time - begin_time
+    test_loss /= len(test_loader.dataset)
+   
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
 
-    L = []
-    L.append('  Step: %s' % format_time(step_time))
-    L.append(' | Tot: %s' % format_time(tot_time))
-    if msg:
-        L.append(' | ' + msg)
+def grad_cam(device,model,images,class_num):
 
-    msg = ''.join(L)
-    sys.stdout.write(msg)
-    for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
-        sys.stdout.write(' ')
+  target_layers = [model.layer4[-1]]
+  input_tensor = images
+  # Note: input_tensor can be a batch tensor with several images!
 
-    # Go back to the center of the bar.
-    for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
-        sys.stdout.write('\b')
-    sys.stdout.write(' %d/%d ' % (current+1, total))
+  # Construct the CAM object once, and then re-use it on many images:
+  cam = GradCAM(model=model, target_layers=target_layers, use_cuda=device)
 
-    if current < total-1:
-        sys.stdout.write('\r')
-    else:
-        sys.stdout.write('\n')
-    sys.stdout.flush()
+  # You can also use it within a with statement, to make sure it is freed,
+  # In case you need to re-create it inside an outer loop:
+  # with GradCAM(model=model, target_layers=target_layers, use_cuda=args.use_cuda) as cam:
+  #   ...
 
-def format_time(seconds):
-    days = int(seconds / 3600/24)
-    seconds = seconds - days*3600*24
-    hours = int(seconds / 3600)
-    seconds = seconds - hours*3600
-    minutes = int(seconds / 60)
-    seconds = seconds - minutes*60
-    secondsf = int(seconds)
-    seconds = seconds - secondsf
-    millis = int(seconds*1000)
+  # We have to specify the target we want to generate
+  # the Class Activation Maps for.
+  # If targets is None, the highest scoring category
+  # will be used for every image in the batch.
+  # Here we use ClassifierOutputTarget, but you can define your own custom targets
+  # That are, for example, combinations of categories, or specific outputs in a non standard model.
 
-    f = ''
-    i = 1
-    if days > 0:
-        f += str(days) + 'D'
-        i += 1
-    if hours > 0 and i <= 2:
-        f += str(hours) + 'h'
-        i += 1
-    if minutes > 0 and i <= 2:
-        f += str(minutes) + 'm'
-        i += 1
-    if secondsf > 0 and i <= 2:
-        f += str(secondsf) + 's'
-        i += 1
-    if millis > 0 and i <= 2:
-        f += str(millis) + 'ms'
-        i += 1
-    if f == '':
-        f = '0ms'
-    return f
+  targets = [ClassifierOutputTarget(class_num)]
+
+  # You can also pass aug_smooth=True and eigen_smooth=True, to apply smoothing.
+  grayscale_cam = cam(input_tensor=input_tensor, targets=targets,aug_smooth=True,eigen_smooth=True)
+
+  # In this example grayscale_cam has only one image in the batch:
+  grayscale_cam = grayscale_cam[0, :]
+  npimg=input_tensor.numpy().transpose(0,2,3,1)
+  visualization = show_cam_on_image(npimg, grayscale_cam)
+  return visualization
+
+def show_image(original_img,grad_cam_visulaisation,pred_lable,target_label,classes):
+  X2 = grad_cam_visulaisation
+  X1 = original_img
+  Y = pred_label
+  #Visualizing CIFAR 10
+  fig, axes1 = plt.subplots(4,5,figsize=(32,32))
+  rnlist = []
+  for j in range(0,4,2):
+      for k in range(5):
+          i=-1
+          while (True):
+            i=np.random.choice(range(len(X1)))        
+            if i not in rnlist:
+              rnlist.append(i)
+              break 
+          axes1[j][k].set_axis_off()
+          axes1[j][k].imshow(X1[i:i+1][0])
+          axes1[j][k].set_title("True: %s\nPredict: %s" % (classes[target_label[i]], classes[Y[i]]))
+          axes1[j+1][k].imshow(X2[i:i+1][0])
+          axes1[j+1][k].set_title("True: %s\nPredict: %s" % (classes[target_label[i]], classes[Y[i]]))
+  
+
+
+def get_mean(trainset):
+  return trainset.data.mean(axis=(0, 1, 2)) / 255, trainset.data.std(axis=(0, 1, 2)) / 255
+   
